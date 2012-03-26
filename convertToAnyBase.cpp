8489a35a72dd6617e970b8003ce2d3feca9e1a54
Lilian A. Moraru <moraru69@yahoo.com>
2012-03-27 01:28:30 +0300

    code refactoring and clean-up

--- convertToAnyBase.cpp
+++ convertToAnyBase.cpp
@@ -3,54 +3,49 @@
 
 using namespace std;
 
-unsigned short L;
-
-unsigned short lungime(char c[]) {
-    register unsigned short i = 0;
-    while(c[++i] != 0) ;
-    return i;
-}
-
 int kToZecimal(char n[], short bazaInitiala) {
     register short i = 0;
+    unsigned short length = 0;
     int s = 0;
-	
-	L = lungime(n);
 
-    while(n[i] != 0) {
-        if(n[i] <= '9')
-            s = s + ((int)n[i] - 48) * pow(bazaInitiala, L - 1 - i);
-        else
-            s = s+((int)n[i] - 87) * pow(bazaInitiala, L - 1 - i);
+    while (n[++length] != (char) NULL); //calculating string length
+    
+    while (n[i] != 0) {
+        if (n[i] <= '9') {
+            s += ((int) n[i] - 48) * pow(bazaInitiala, length - 1 - i);
+        } else {
+            s += ((int) n[i] - 87) * pow(bazaInitiala, length - 1 - i);
+        }
         i++;
     }
     return s;
 }
 
 inline double abs(double n) {
-    if(n < 0) return n * (-1);
-    else
-    return n;
+    if (n < 0) {
+        return n * (-1);
+    } else {
+        return n;
+    }
 }
 
 void ZecimalToK(register int nr, int bazaFinala) {
-        const string numerele("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"); //pana in baza 36
-        bool is_neg = nr < 0;
-        string rezultatul;
+    const string numerele("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"); //pana in baza 36
+    bool is_neg = nr < 0;
+    string rezultatul;
 
-        for (; nr; nr /= bazaFinala)
-            rezultatul.insert(rezultatul.begin(), numerele[abs(nr % bazaFinala)]);
+    for (; nr; nr /= bazaFinala)
+        rezultatul.insert(rezultatul.begin(), numerele[abs(nr % bazaFinala)]);
 
-        if (is_neg)
-            rezultatul.insert(rezultatul.begin(), '-');
+    if (is_neg)
+        rezultatul.insert(rezultatul.begin(), '-');
 
-        cout << rezultatul << endl; //afisarea rezultatului final
+    cout << rezultatul << endl; //afisarea rezultatului final
 }
 
-inline void Conversie(char nr[], int bazaInitiala, int bazaFinala) { 
-	ZecimalToK(kToZecimal(nr, bazaInitiala), bazaFinala);
+inline void Conversie(char nr[], int bazaInitiala, int bazaFinala) {
+    ZecimalToK(kToZecimal(nr, bazaInitiala), bazaFinala);
 }
-
 
 int main(void) {
     char Numarul[100];
